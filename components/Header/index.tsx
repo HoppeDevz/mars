f559c140314df15a783bce2c6922e46be446c764
Gabriel Hoppe <hoppecsgonet2@gmail.com>
2021-12-30 18:30:42 -0300

    🐐 create header component

---
+++ components/Header/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Image from 'next/image';
+
+import NasaLogo from '../../assets/nasa-logo-font-white-70x20.png'
+
+const Header: React.FC = () => {
+
+    function goToNasaApi() {
+
+        window.open("https://api.nasa.gov/")
+    }
+
+    function goToGithubRepo() {
+
+        window.open("https://github.com/HoppeDevz/")
+    }
+
+    return(
+        <header className="w-100 py-10 flex flex-row bg-#080808 justify-between items-center">
+
+            <div className="px-10">
+
+                <Image 
+                    width="70"
+                    height="20"
+                    src={NasaLogo} 
+                />
+            </div>
+            
+
+            <div className="px-10 flex flex-row">
+                <p onClick={goToGithubRepo} className="cursor-pointer hover:text-white text-slate-400 font-bold mx-2">Github</p>
+                <p onClick={goToNasaApi} className="cursor-pointer hover:text-white text-slate-400 font-bold mx-2">NASA API</p>
+            </div>
+           
+        </header>
+    )
+}
+
+export default Header;
